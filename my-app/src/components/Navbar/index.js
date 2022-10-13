7739be51adfd88f90c7f017474cbd595865ac639
Shiying Cai <cai2433717514@gmail.com>
2022-10-13 03:07:16 -0400

    update in frontend

---
+++ my-app/src/components/Navbar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/Home' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/Edit' activeStyle>
+            Edit
+          </NavLink>
+          <NavLink to='/Upload' activeStyle>
+            Upload Record
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
