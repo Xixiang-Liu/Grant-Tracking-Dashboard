ab4aeea8063b1261db5ebb47a3f228dcba6ab833
Xixiang-Liu <xxl@ad.unc.edu>
2022-10-24 23:33:34 -0400

    created table for the planetscale database

--- my-app/src/mysql_database/create_table.js
+++ my-app/src/mysql_database/create_table.js
@@ -1,21 +1,40 @@
+// the url for my database on planetscale
+DATABASE_URL='mysql://wq7ze3yylpob237bz1xi:pscale_pw_cIe5yy8VtcFCCUcUyVksF3P39v9YXr9cNS5OzBQINMK@us-east.connect.psdb.cloud/grant_tracking?ssl={"rejectUnauthorized":true}'
+
 // require needed stuffs
-var mysql = require('mysql');
+require('dotenv').config()
+const mysql = require('mysql2')
 
-// connection information
-var con = mysql.createConnection({
-  host: "localhous-east.connect.psdb.cloudst",
-  user: "wq7ze3yylpob237bz1xi",
-  password: "pscale_pw_cIe5yy8VtcFCCUcUyVksF3P39v9YXr9cNS5OzBQINMK",
-  database: "grant_tracking"
-});
+// create connection
+const connection = mysql.createConnection(DATABASE_URL)
+console.log('Connected to PlanetScale!')
 
-// create the table
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-  con.query(sql, function (err, result) {
+// create table
+connection.connect(function(err) {
     if (err) throw err;
-    console.log("Table created");
-  });
+    console.log("Connected!");
+    
+    // my query
+    var sql = `CREATE TABLE transactions (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        date DATETIME, 
+        vendor VARCHAR(100), 
+        amount Decimal(6, 2), 
+        category VARCHAR(100), 
+        account VARCHAR(100), 
+        program VARCHAR(100), 
+        account_group VARCHAR(100), 
+        budget Decimal(6, 2), 
+        description VARCHAR(255)
+    )`;
+
+    // do the query
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+
 });
+
+// end connection
+//connection.end()
