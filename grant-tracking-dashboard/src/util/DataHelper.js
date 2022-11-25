7180d5da74a33c235ab0eb736fd5956da1753bfc
Xixiang-Liu <xxl@ad.unc.edu>
2022-11-25 12:28:36 -0500

    change BASE_SERVER_URL in DataHelper.js

--- grant-tracking-dashboard/src/util/DataHelper.js
+++ grant-tracking-dashboard/src/util/DataHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_SERVER_URL = 'http://localhost:3001/'
+export const BASE_SERVER_URL = 'https://unrivaled-llama-9b0278.netlify.app/'
 
 export const handleInsertDB = (params) => {
   return new Promise((resolve, reject) => {
